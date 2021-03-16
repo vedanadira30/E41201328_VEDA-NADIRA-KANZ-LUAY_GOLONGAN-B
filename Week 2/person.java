58b9ec46cbd25178cb5023ab5bf9be563dc3d669
vedanadira30 <80373053+vedanadira30@users.noreply.github.com>
2021-03-16 14:34:11 +0700

    Create person.java

---
+++ Week 2/person.java
@@ -0,0 +1,33 @@
+// FIRST TASK FOR WORKSHOP SISTEM BERBASIS DESKTOP 2021 //
+package tugas_1;
+
+/**
+ *
+ * @author VEDA NADIRA KANZ LUAY E41201328
+ */
+public class person {
+
+    public static void main(String[] args) {
+    
+        // Initialize the Variabels//
+       
+        String fName, lName, stuStatus;
+        fName = "Lisa";
+        lName = "Palombo";
+        stuStatus = "Active";
+       
+        int stuId;
+        stuId = (123456789);
+        
+        // Show the Program //
+        
+        System.out.println("Students Name : "+fName + "  " + lName);
+        System.out.println("Students ID : "+stuId);
+        System.out.println("Students Status : "+stuStatus);
+             
+    }
+    
+}
+
+
+
